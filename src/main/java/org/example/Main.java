b1e9be87ab22303c8bcd74cedff633dfaea0097f
rookiecoder34 <ayaydintolga@gmail.com>
2026-02-02 22:17:38 +0300

    First asignmnet completed.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Person person1=new Person("TOLGA","AYAYDIN",15);
+        Person person = new Person("John", "Doe", 20);
+        System.out.println(person1.isTeen());
+
     }
 }
---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,54 @@
+package org.example;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double height;
+    private double weight;
+    private String gender;
+
+    public Person(String firstName, String lastName, int age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+    public Person(String firstName, String lastName, int age, double height,
+                  double weight, String gender) {
+        this(firstName, lastName, age);
+        this.height = height;
+        this.weight = weight;
+        this.gender = gender;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isTeen() {
+        boolean teen;
+
+        if (age > 13 && age<19) {
+            teen = true;
+        }else {
+            teen = false;
+        }
+        return teen;
+    }
+    public double getHeight() {
+        return height;
+    }
+    public double getWeight() {
+        return weight;
+    }
+    public String getGender() {
+        return gender;
+    }
+
+}
